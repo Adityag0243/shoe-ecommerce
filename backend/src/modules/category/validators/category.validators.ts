d3b755ce346cb2a8c303c5f8be26c9fa3ec35989
Aditya Gupta <mr.adityag123@gmail.com>
2026-04-05 20:31:53 +0530

    fix: design

--- backend/src/modules/category/validators/category.validators.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-export const createCategorySchema = z.object({
-    body: z.object({
-        name: z.enum(['shoes', 'clogs']),
-    }),
-    query: z.any().optional(),
-    params: z.any().optional(),
-});
--- backend/src/swagger/swagger.router.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import swaggerUi from 'swagger-ui-express';
-
-import { swaggerSpec } from './swagger.js';
-
-export const swaggerRouter = Router();
-
-swaggerRouter.use('/', swaggerUi.serve);
-swaggerRouter.get('/', swaggerUi.setup(swaggerSpec));
